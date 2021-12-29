e88b464895f54a987c4229a0ebbf5723eca85115
Anubis201 <anubisanubias@gmail.com>
2021-12-29 19:24:13 +0100

    change

--- src/app/modules/all-articles/parties/parties.component.ts
+++ src/app/modules/all-articles/parties/parties.component.ts
@@ -1,16 +1,28 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ArticlesTypesEnum } from 'src/app/models/articles/enums/articles-types.enum';
+import { ArticlesService } from 'src/app/services/collections/articles/articles.service';
 
 @Component({
   selector: 'app-parties',
   templateUrl: './parties.component.html',
   styleUrls: ['.././all-articles.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PartiesComponent implements OnInit {
+  constructor(private articlesService: ArticlesService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.getArticles()
   }
 
+  getArticles() {
+    this.articlesService
+      .getArticles(ArticlesTypesEnum.PoliticalParties, 4)
+      .subscribe({
+        next: articles => {
+          articles.forEach(value => { console.log(value.data()) })
+        },
+        error: () => {},
+      });
+  }
 }
--- src/app/services/collections/articles/articles.service.ts
+++ src/app/services/collections/articles/articles.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { from } from 'rxjs';
 import { ArticleModel } from 'src/app/models/articles/article.model';
+import { ArticlesTypesEnum } from 'src/app/models/articles/enums/articles-types.enum';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +15,11 @@ export class ArticlesService {
 
   addArticle(data: ArticleModel) {
     return from(this.firestore.collection('articles').add(data))
+  }
+
+  getArticles(type: ArticlesTypesEnum, limit: number) {
+    const ref = this.firestore.collection('articles').ref
+
+    return from(ref.where('type', '==', type).limit(limit).get())
   }
 }
