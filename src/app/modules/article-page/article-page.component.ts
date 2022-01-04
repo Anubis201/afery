fb0ac9b012c7ac0cdbdee26f547387f32f77bcfd
Anubis201 <anubisanubias@gmail.com>
2022-01-04 16:20:21 +0100

    ok

--- src/app/modules/article-page/article-page.component.ts
+++ src/app/modules/article-page/article-page.component.ts
@@ -5,6 +5,7 @@ import { ArticleModel } from 'src/app/models/articles/article.model';
 import { ArticlesTypesEnum } from 'src/app/models/articles/enums/articles-types.enum';
 import { PartiesEnum } from 'src/app/models/articles/enums/parties.enum';
 import { ArticlesService } from 'src/app/services/collections/articles/articles.service';
+import { CommentsService } from 'src/app/services/collections/comments/comments.service';
 
 @Component({
   selector: 'app-article-page',
@@ -22,12 +23,17 @@ export class ArticlePageComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private articlesService: ArticlesService,
+    private commentsService: CommentsService,
   ) { }
 
   ngOnInit() {
     this.route.params.subscribe(({ articleId }) => {
       this.getData(articleId)
     })
+  }
+
+  handleAddComment(comment: { name: string, comment: string }) {
+    this.commentsService.addComents()
   }
 
   private getData(articleId: string) {
