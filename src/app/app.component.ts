8a4727f2cabb12c231416c71c115a8c208e3716a
Anubis201 <anubisanubias@gmail.com>
2022-01-07 16:42:09 +0100

    added menu animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
@@ -6,6 +7,21 @@ import { BehaviorSubject } from 'rxjs';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    trigger('menu', [
+      transition (':enter', [
+        style ({ transform: 'translateX(-100vw)' }),
+        animate ('200ms',
+          style ({ transform: 'translateX(0)' }),
+        ),
+      ]),
+      transition (':leave', [
+        animate ('200ms',
+          style ({ transform: 'translateX(-100vw)' }),
+        ),
+      ]),
+    ])
+  ]
 })
 export class AppComponent {
   isOpenMenu = new BehaviorSubject<boolean>(false)
