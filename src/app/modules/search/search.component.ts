11b7fb75d34e169465be5ae283e729ace654615b
Anubis201 <anubisanubias@gmail.com>
2022-01-29 15:55:17 +0100

    ok

--- src/app/modules/search/search.component.ts
+++ src/app/modules/search/search.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  styleUrls: ['./search.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent {
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
