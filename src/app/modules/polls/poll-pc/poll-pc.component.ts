56645c01cec599c84ca74532d5a9f8a2d6621a69
Anubis201 <anubisanubias@gmail.com>
2022-02-10 20:35:14 +0100

    ok

--- src/app/modules/polls/poll-pc/poll-pc.component.ts
+++ src/app/modules/polls/poll-pc/poll-pc.component.ts
@@ -1,5 +1,9 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { PollModel } from 'src/app/models/polls/poll.model';
+import * as d3 from 'd3';
+import { PartyCharModel } from 'src/app/models/articles/party-char.model';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+import { PartiesEnum } from 'src/app/models/articles/enums/parties.enum';
 
 @Component({
   selector: 'app-poll-pc',
@@ -9,5 +13,80 @@ import { PollModel } from 'src/app/models/polls/poll.model';
 })
 export class PollPcComponent {
   @Input() poll: PollModel
+  @Input() idSvg: string
 
+  @ViewChild('image') image: ElementRef;
+
+  private margin = 30;
+  private width;
+  private height = 200 - (this.margin * 2);
+
+  ngAfterViewInit() {
+    this.draw(this.poll.parties);
+  }
+
+  private draw(data: PartyCharModel[]) {
+    this.width = parseInt(this.image.nativeElement.offsetWidth, 10);
+    const isMobileVersion = this.width <= 330;
+
+    const xScale = d3
+      .scaleBand()
+      .range([0, this.width])
+      .domain(data.map(d => d.party as unknown as string))
+      .padding(0.2);
+
+    const yScale = d3
+      .scaleLinear()
+      .domain([0, 50])
+      .range([this.height, 0]);
+
+    const chartContainer = d3
+      .select('#' + this.idSvg)
+      .attr('width', this.width)
+      .classed('chart-container', true)
+      .attr('height', this.height + (this.margin * 2))
+
+    const chart = chartContainer.append('g');
+
+    chart
+      .append('g')
+      .attr('transform', 'translate(0,' + this.height + ')')
+      .call(d3.axisBottom(xScale))
+      .selectAll('.tick')
+      .append('svg:image')
+      .attr('xlink:href', party => `/assets/icons/parties/${PartiesEnum[party as PartiesEnum]}.png`)
+      .attr('height', isMobileVersion ? 25 : 30)
+      .attr('width', isMobileVersion ? 30 : 40)
+      .attr('y', 10)
+      .attr('x', isMobileVersion ? -14 : -20);
+
+    chart
+      .select('g')
+      .selectAll('text')
+      .remove();
+
+    chart
+      .selectAll('.bar')
+      .data(data)
+      .enter()
+      .append('rect')
+      .classed('bar', true )
+      .attr('x', d => xScale(d.party as unknown as string))
+      .attr('y', d => yScale(d.percentage))
+      .attr('width', xScale.bandwidth())
+      .attr('height', d => this.height - yScale(d.percentage))
+      .attr('fill', '#db00db');
+
+    chart
+      .selectAll('.label')
+      .data(data)
+      .enter()
+      .append('text')
+      .text(d => d.percentage as unknown as string + '%')
+      .attr('x', d => xScale(d.party as unknown as string) + xScale.bandwidth() / 2)
+      .attr('y', d => yScale(d.percentage) - 15)
+      .attr('fill', 'white')
+      .attr('font-weight', 500)
+      .attr('text-anchor', 'middle');
+  }
 }
