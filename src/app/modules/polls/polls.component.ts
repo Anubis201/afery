8c8311b828fba7e14a16f36e47b77265c00155c2
Anubis201 <anubisanubias@gmail.com>
2022-02-19 15:21:47 +0100

    save

--- src/app/modules/polls/polls.component.ts
+++ src/app/modules/polls/polls.component.ts
@@ -6,6 +6,15 @@ import { PollModel } from 'src/app/models/polls/poll.model';
 import { PollsService } from 'src/app/services/collections/polls/polls.service';
 import { ConvertEnum } from 'src/app/services/global/support-functions/convert-enum';
 
+interface SectionPollModel {
+  polls: PollModel[]
+  lastPollsnapshot: any
+  isLastPage: boolean
+  isLoading: boolean
+}
+
+type DataType = Record<PollDataEnum, SectionPollModel>;
+
 @Component({
   selector: 'app-polls',
   templateUrl: './polls.component.html',
@@ -13,12 +22,9 @@ import { ConvertEnum } from 'src/app/services/global/support-functions/convert-e
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PollsComponent implements OnInit {
-  allPolls = new BehaviorSubject<PollModel[]>([])
-  isLoading = new BehaviorSubject<boolean>(false)
+  data = new BehaviorSubject<DataType>(this.createPage() as DataType)
   showMore = new BehaviorSubject<boolean>(false)
 
-  private lastItemSnapshot = null
-  readonly PollDataArray = ConvertEnum(PollDataEnum, 'string')
   private readonly limit = 6
 
   constructor(
@@ -28,23 +34,19 @@ export class PollsComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.PollDataArray.forEach(element => {
-      this.getPolls(element);
-    })
-
     this.metaTags();
   }
 
-  getPolls(pollData: string, isMore = false) {
-    this.isLoading.next(true);
-    this.pollsService.getPolls(this.limit + 1, isMore, this.lastItemSnapshot, PollDataEnum[pollData]).subscribe({
+  getPolls(pollData: PollDataEnum, isMore = false) {
+    this.pollsService.getPolls(this.limit + 1, isMore, this.data.value[pollData].lastPollsnapshot, pollData).subscribe({
       next: docs => {
         let data: PollModel[] = [];
         let i = 0;
+        let snapshot: any;
 
         docs.forEach(d => {
           data.push({ ...d.data() as PollModel, when: (d.data() as any).when.toDate(), id: d.id })
-          if (this.limit - 1 === i) this.lastItemSnapshot = d;
+          if (this.limit - 1 === i) snapshot = d;
           i++;
         })
 
@@ -55,15 +57,27 @@ export class PollsComponent implements OnInit {
           this.showMore.next(false);
         }
 
-        this.allPolls.next([
-          ...this.allPolls.value,
-          ...data,
-        ]);
-        this.isLoading.next(false);
+        this.data.next({
+          ...this.data.value,
+          [type]: {
+            articles: orderChange ? articles : [...this.data.value[type].articles, ...articles],
+            lastArticlesnapshot: snap,
+            order: orderChange || this.data.value[type].order,
+            isLastPage: isLimit,
+            isLoading: false
+          }
+        })
       },
-      error: err => {
-        console.log(err)
-        this.isLoading.next(false)
+      error: () => this.changeLoading(pollData, false)
+    })
+  }
+
+  private changeLoading(type: PollDataEnum, isLoading: boolean) {
+    this.data.next({
+      ...this.data.value,
+      [type]: {
+        ...this.data.value[type],
+        isLoading,
       }
     })
   }
@@ -71,5 +85,20 @@ export class PollsComponent implements OnInit {
   private metaTags() {
     this.titleService.setTitle('Sondaże i badania');
     this.meta.updateTag({ name:'description', content:'Tu znajdziesz najnowsze sondaże polskich partii. Zapraszam na inne strony, gdzie zobaczysz afery naszej "niesamowitej" polityki.' }, "name='description'");
+  }
+
+  private createPage() {
+    const object = {}
+
+    ConvertEnum(PollDataEnum, 'number').forEach((value: PollDataEnum) => {
+      object[value] = {
+        polls: [],
+        lastPollsnapshot: null,
+        isLastPage: false,
+        isLoading: false,
+      }
+    })
+
+    return object;
   }
 }
