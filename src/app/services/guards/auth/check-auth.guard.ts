be56337b1d0830725cfb405f7c8d9e081bb512e2
Anubis201 <anubisanubias@gmail.com>
2022-02-25 13:43:09 +0100

    fix admin control

--- src/app/services/guards/auth/check-auth.guard.ts
+++ src/app/services/guards/auth/check-auth.guard.ts
@@ -1,22 +1,22 @@
 import { Injectable } from '@angular/core'
-import { AngularFireAuth } from '@angular/fire/compat/auth'
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router'
+import { CanActivate, Router, UrlTree } from '@angular/router'
 import { Observable } from 'rxjs'
 import { map } from 'rxjs/operators'
+import { UserService } from '../../global/user/user.service'
 
 @Injectable({
   providedIn: 'root'
 })
 export class CheckAuthGuard implements CanActivate {
   constructor(
-    private fireAuth: AngularFireAuth,
     private router: Router,
+    private userService: UserService,
   ) {}
 
   canActivate(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      return this.fireAuth.authState.pipe(
-        map(user => {
-          if (!user) {
+      return this.userService.isAdmin.pipe(
+        map(isAdmin => {
+          if (!isAdmin) {
             return this.router.parseUrl('admin/auth');
           }
           return true;
