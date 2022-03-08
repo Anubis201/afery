399b37a1251f8005a932f69094d904369a55cee0
Anubis201 <anubisanubias@gmail.com>
2022-03-08 22:35:48 +0100

    added mobile nav

--- src/app/modules/main/mobile-bottom/mobile-bottom.component.ts
+++ src/app/modules/main/mobile-bottom/mobile-bottom.component.ts
@@ -1,25 +1,59 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-mobile-bottom',
   templateUrl: './mobile-bottom.component.html',
   styleUrls: ['./mobile-bottom.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MobileBottomComponent {
+export class MobileBottomComponent implements OnInit {
   items = [
     {
       icon: 'forum',
       title: 'Bulwar',
+      href: '/bulwar/dyskusje',
+      active: false
     },
     {
       icon: 'house',
       title: 'Afery',
+      href: '/',
+      active: false
     },
     {
       icon: 'poll',
       title: 'Sondaże',
+      href: '/sondaze',
+      active: false
     },
   ]
 
+  constructor(
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        const pathname = location.pathname;
+        this.setALLActiveTofalse();
+
+        switch(pathname) {
+          case '/':
+            this.items[1].active = true;
+            break
+          case '/bulwar/dyskusje':
+            this.items[0].active = true;
+            break
+          case '/sondaze':
+            this.items[2].active = true;
+            break;
+        }
+      }
+    })
+  }
+
+  private setALLActiveTofalse() {
+    this.items = this.items.map(item => ({ ...item, active: false }))
+  }
 }
