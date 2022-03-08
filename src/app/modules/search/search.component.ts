e9d09ce001b6fd6827b5b85d633cd4c88908e670
Anubis201 <anubisanubias@gmail.com>
2022-03-08 10:21:25 +0100

    better search

--- src/app/modules/search/search.component.ts
+++ src/app/modules/search/search.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl } from '@angular/forms';
 import { Meta, Title } from '@angular/platform-browser';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, debounceTime, distinctUntilChanged, filter, map, switchMap, tap } from 'rxjs';
 import { ArticleModel } from 'src/app/models/articles/article.model';
 import { ArticlesService } from 'src/app/services/collections/articles/articles.service';
 
@@ -12,10 +12,7 @@ import { ArticlesService } from 'src/app/services/collections/articles/articles.
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SearchComponent implements OnInit {
-  form = new FormGroup({
-    searched: new FormControl('')
-  })
-
+  searchValue = new FormControl('')
   articles = new BehaviorSubject<ArticleModel[]>([])
   isLoading = new BehaviorSubject<boolean>(false)
 
@@ -27,31 +24,39 @@ export class SearchComponent implements OnInit {
 
   ngOnInit() {
     this.metaTagsAndTitle();
-  }
 
-  search() {
-    this.isLoading.next(true);
-    this.articlesService.likeQuery(this.form.get('searched').value).subscribe({
-      next: docs => {
-        let articles: ArticleModel[] = [];
+    this.searchValue.valueChanges
+      .pipe(
+        filter(value => value.length >= 2),
+        debounceTime(500),
+        distinctUntilChanged(),
+        tap(() => this.isLoading.next(true)),
+        switchMap(value =>
+          this.articlesService.likeQuery(value).pipe(
+            map(docs => {
+              let articles: ArticleModel[] = [];
 
-        docs.forEach(d =>{
-          articles.push({
-            ...d.data() as ArticleModel,
-            id: d.id,
-            createDate: (d.data() as any).createDate.toDate()
-          });
-        })
+              docs.forEach(d =>{
+                articles.push({
+                  ...d.data() as ArticleModel,
+                  id: d.id,
+                  createDate: (d.data() as any).createDate.toDate()
+                });
+              })
 
+              return articles;
+            })
+          )
+        )
+      )
+      .subscribe(articles => {
         this.articles.next(articles);
         this.isLoading.next(false);
-      }
-    })
+      })
   }
 
   private metaTagsAndTitle() {
     this.titleService.setTitle('Afery - Szukaj afer');
-
     this.meta.updateTag({ name:'description', content:'Chcesz znaleźć jakąś afere? Zapraszamy do wyszukiwarki.' }, "name='description'");
   }
 }
