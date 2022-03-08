369dac520bbd6755d6a6f743dee882db30197327
Anubis201 <anubisanubias@gmail.com>
2022-03-08 12:09:08 +0100

    update

--- src/app/modules/account/account.component.ts
+++ src/app/modules/account/account.component.ts
@@ -1,11 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+interface itemsModel {
+  title: string
+  desc: string
+  href: string
+}
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.scss']
+  styleUrls: ['./account.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AccountComponent implements OnInit {
+  items: itemsModel[] = [
+    {
+      title: '',
+      desc: '',
+      href: ''
+    }
+  ]
 
   constructor() { }
 
--- src/app/modules/account/navigation-item/navigation-item.component.ts
+++ src/app/modules/account/navigation-item/navigation-item.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-navigation-item',
   templateUrl: './navigation-item.component.html',
-  styleUrls: ['./navigation-item.component.scss']
+  styleUrls: ['./navigation-item.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class NavigationItemComponent implements OnInit {
+export class NavigationItemComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
