250b2a15355e6f615b18f2098ef1e0b06fdf6665
Anubis201 <anubisanubias@gmail.com>
2022-03-15 19:42:38 +0100

    fix

--- src/app/models/articles/comment.model.ts
+++ src/app/models/articles/comment.model.ts
@@ -1,7 +1,6 @@
 export interface CommentModel {
   articleId?: string
   pollId?: string
-  name: string
   text: string
   date: Date
   isNew: boolean
@@ -9,7 +8,6 @@ export interface CommentModel {
   likes: number
   authorId: string
   dislikes: number
-  avatar?: string
   countAnswers?: number
   commentId?: string // tylko gdy jest to odpowiedz na jakis komentarz
   id?: string // trzeba to ustawić recznie podczas pobierania komentarza ustawic mu id dokumenu
