8fb5d33185dfa6e1d634c329670c327f8e654eb6
Anubis201 <anubisanubias@gmail.com>
2022-03-16 01:19:44 +0100

    Revert "fix"
    This reverts commit 250b2a15355e6f615b18f2098ef1e0b06fdf6665.

--- src/app/models/articles/comment.model.ts
+++ src/app/models/articles/comment.model.ts
@@ -1,6 +1,7 @@
 export interface CommentModel {
   articleId?: string
   pollId?: string
+  name: string
   text: string
   date: Date
   isNew: boolean
@@ -8,6 +9,7 @@ export interface CommentModel {
   likes: number
   authorId: string
   dislikes: number
+  avatar?: string
   countAnswers?: number
   commentId?: string // tylko gdy jest to odpowiedz na jakis komentarz
   id?: string // trzeba to ustawić recznie podczas pobierania komentarza ustawic mu id dokumenu
