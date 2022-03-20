4fc053ebb47147cdbc2726cbb2f90645d604fdbb
Anubis201 <anubisanubias@gmail.com>
2022-03-20 18:49:18 +0100

    update

--- src/app/modules/all-articles/poll-bar/poll-bar.component.ts
+++ src/app/modules/all-articles/poll-bar/poll-bar.component.ts
@@ -1,9 +1,15 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { BehaviorSubject } from 'rxjs';
 import { PartiesEnum } from 'src/app/models/articles/enums/parties.enum';
+import { PartyCharModel } from 'src/app/models/articles/party-char.model';
 import { PollModel } from 'src/app/models/polls/poll.model';
 import { PollsService } from 'src/app/services/collections/polls/polls.service';
+
+interface SomethingModel {
+  party: PartiesEnum,
+  percentage: number,
+  difference: number
+}
 
 @Component({
   selector: 'app-poll-bar',
@@ -13,6 +19,7 @@ import { PollsService } from 'src/app/services/collections/polls/polls.service';
 })
 export class PollBarComponent implements OnInit {
   poll = new BehaviorSubject<PollModel>(null)
+  data = new BehaviorSubject<SomethingModel[]>([])
 
   readonly PartiesEnum = PartiesEnum
 
@@ -24,13 +31,30 @@ export class PollBarComponent implements OnInit {
     this.getNewestPoll();
   }
 
+  private previousPoll() {
+    this.pollsService.getPreviousPoll(this.poll.value.surveying, this.poll.value.when).subscribe({
+      next: data => {
+        this.setData(data);
+      }
+    })
+  }
+
   private getNewestPoll() {
     this.pollsService.getNewestPollParty().subscribe({
       next: docs => {
         docs.forEach(doc => {
           this.poll.next(doc.data() as PollModel);
         })
+        this.previousPoll();
       },
     })
+  }
+
+  private setData(poll: PollModel) {
+    this.data.next((this.poll.value.items as unknown as PartyCharModel[]).map(current => ({
+      party: current.party,
+      difference: !poll ? 0 : parseFloat((poll ? current.percentage - (poll.items as unknown as PartyCharModel[]).find(v => v.party === current.party).percentage : current.percentage).toFixed(1)),
+      percentage: current.percentage,
+    })));
   }
 }
