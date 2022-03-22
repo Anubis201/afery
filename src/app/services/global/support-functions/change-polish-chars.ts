49be19b6c0ad9dc326deab96f352cb91f103fae2
Anubis201 <anubisanubias@gmail.com>
2022-03-22 17:41:25 +0100

    update

--- src/app/services/global/support-functions/change-polish-chars.ts
+++ src/app/services/global/support-functions/change-polish-chars.ts
@@ -1,13 +1,15 @@
 export const ChangePolishChars = (str: string) => {
-  str = str.replace('ę','e');
-  str = str.replace('ó','o');
-  str = str.replace('ą','a');
-  str = str.replace('ś','s');
-  str = str.replace('ł','l');
-  str = str.replace('ż','z');
-  str = str.replace('ź','z');
-  str = str.replace('ć','c');
-  str = str.replace('ń','n');
+  str = str.replace('ę', 'e');
+  str = str.replace('ó', 'o');
+  str = str.replace('ą', 'a');
+  str = str.replace('ś', 's');
+  str = str.replace('ł', 'l');
+  str = str.replace('ż', 'z');
+  str = str.replace('ź', 'z');
+  str = str.replace('ć', 'c');
+  str = str.replace('ń', 'n');
+  str = str.replace(',', '');
+  str = str.replace('.', '');
 
   return str.replace(/\s/g, '-').toLowerCase();
 }
