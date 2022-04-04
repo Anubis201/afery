d7516b882a2e8a17728cd91fd622c377a3b774ac
Anubis201 <anubisanubias@gmail.com>
2022-04-04 12:15:57 +0200

    update

--- src/app/modules/election-test/election-test.component.ts
+++ src/app/modules/election-test/election-test.component.ts
@@ -3,7 +3,6 @@ import { Title, Meta } from '@angular/platform-browser';
 import { BehaviorSubject } from 'rxjs';
 import { PartiesEnum } from 'src/app/models/articles/enums/parties.enum';
 import { QuestionModel } from 'src/app/models/polls/question.model';
-import { ResultElectionType } from 'src/app/models/polls/result-election.model';
 import { showAnimation } from 'src/app/services/animations/others.animations';
 import { QuestionsData } from 'src/app/services/global/data/questions';
 import { ConvertEnum } from 'src/app/services/global/support-functions/convert-enum';
@@ -17,10 +16,9 @@ import { UserService } from 'src/app/services/global/user/user.service';
   animations: [showAnimation],
 })
 export class ElectionTestComponent implements OnInit {
-  isStart = new BehaviorSubject<boolean>(false)
   lvl = new BehaviorSubject<number>(1)
   questions = new BehaviorSubject<QuestionModel[]>(QuestionsData)
-  isEnd = new BehaviorSubject<boolean>(false)
+  testMode = new BehaviorSubject<'menu' | 'lvl' | 'result' | 'checkResult'>('menu')
   isAllQuestionChoosed = new BehaviorSubject<boolean>(false)
   result = new BehaviorSubject<any[]>(this.createResultObject() as any[])
 
@@ -63,11 +61,11 @@ export class ElectionTestComponent implements OnInit {
 
     const sortableResult = Object.entries(result).sort(([, a], [, b]) => (a - b))
     this.result.next(sortableResult.map(ele => ({ party: ele[0], points: ele[1] })).reverse() as any)
-    this.isEnd.next(true);
+    this.testMode.next('result');
   }
 
   handleStart() {
-    this.isStart.next(true);
+    this.testMode.next('lvl');
   }
 
   onChangeAnswer({ answerIndex, isChoosed }: { answerIndex: number, isChoosed: boolean }) {
