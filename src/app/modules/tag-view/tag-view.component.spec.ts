28b1372e8505d559c86fac22e8c6cec9dce6f381
Anubis201 <anubisanubias@gmail.com>
2022-04-16 17:40:47 +0200

    update

--- src/app/modules/tag-view/tag-view.component.spec.ts
+++ src/app/modules/tag-view/tag-view.component.spec.ts
@@ -1,14 +1,42 @@
+import { Overlay } from '@angular/cdk/overlay';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AngularFireModule } from '@angular/fire/compat';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 import { TagViewComponent } from './tag-view.component';
 
 describe('TagViewComponent', () => {
   let component: TagViewComponent;
   let fixture: ComponentFixture<TagViewComponent>;
+  const fakeRoute = {
+    provide: ActivatedRoute,
+    useValue: {
+        snapshot: {
+            params: {
+                get(): Observable<any> {
+                    return new Observable();
+                }
+            }
+        }
+    }
+  }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TagViewComponent ]
+      declarations: [ TagViewComponent ],
+      providers: [
+        fakeRoute,
+        MatSnackBar,
+        Overlay,
+      ],
+      imports: [
+        RouterTestingModule,
+        AngularFireModule.initializeApp(environment.firebase),
+      ]
     })
     .compileComponents();
   });
