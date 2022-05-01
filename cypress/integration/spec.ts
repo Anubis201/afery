5f198d989c19e16c6a7825d8b8093da5d4c151db
Anubis201 <anubisanubias@gmail.com>
2022-05-01 20:55:19 +0200

    ok

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -1,7 +1,7 @@
-describe('My First Test', () => {
+describe('check start page', () => {
   it('Visits the initial project page', () => {
     cy.visit('/')
-    cy.contains('Welcome')
-    cy.contains('sandbox app is running!')
+    cy.title().should('equal', 'Polityka i inne tematy - Afery')
+    cy.get('meta[name="description"]').should('have.attr', 'content', 'Afery naszych polityków, partii i innych osobistości. Mamy miejsce, gdzie możesz pogadać o wszystkim bez żadnej cenzury oraz miejsce, gdzie zbieramy sondaże (nie tylko partii).')
   })
 })
